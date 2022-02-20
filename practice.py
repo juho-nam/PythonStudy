d882f5452aac646c775656113b8fbfd41f1ba970
juho-nam <juhonam.dev@gamil.com>
2022-02-20 12:30:28 +0900

    Simple mathematical expressions

--- practice.py
+++ practice.py
@@ -1,18 +1,12 @@
-# 간단한 수식
-print(2 + 3 * 4) # 14
-print((2 + 3) * 4) # 20
-number = 2 + 3 * 4 #14
-print(number)
-number = number + 2 #16
-print(number)
-number += 2 # 18
-print(number)
-number *= 2 # 36
-print(number)
-number /= 2 # 18
-print(number)
-number -= 2 #16
-print(number)
+# 숫자처리함수
+print(abs(-5)) # -5의 절대값
+print(pow(4,2)) # 4^2 = 16
+print(max(5, 12)) # 5와 12 중 최대값. 12
+print(min(5, 12)) # 5와 12 중 최소값.  5
+print(round(3.14)) # 반올림. 3
+print(round(4.99)) # 5
 
-number %= 2 # 0
-print(number)
+from math import *
+print(floor(4.99)) # 내림. 4
+print(ceil(3.14)) # 올림. 4
+print(sqrt(16)) # 제곱근. 4.0
