72cbf8c668ff2256a54eee19fae34c81d7562ae0
juho-nam <juhonam.dev@gamil.com>
2022-02-20 12:20:19 +0900

    Operator

--- practice.py
+++ practice.py
@@ -1,12 +1,32 @@
-# 변수
-animal = "고양이"
-name = "해피"
-age = 4
-hobby = "낮잠"
-is_adult = age >= 3 # 나이가 3살 이상이면 어른으로 본다
+# 연산자
+print(1+1) #2
+print(3-2) #1
+print(5*2) #10
+print(6/3) #2.0
 
-print("우리집 " + animal + "의 이름은 " + name + "예요.")
-hobby = "공놀이" # 취미가 바뀌었으므로 공놀이 출력
-print(name, "는 ", age, "살이며, ", hobby, "을 아주 좋아해요.")
-# '+' 대신 ',' 사용가능. 이 경우 정수 바로 사용가능. 단, ',' 이후엔 반드시 빈칸 포함됨
-print(name + "는 어른일까요? " + str(is_adult))
+print(2**3) # 2^3 = 8
+print(5%3) # 5/3의 나머지 = 2
+print(10%3) # 1
+print(5//3) # 5/3 의 몫 = 1
+print(10//3) # 3
+
+print(10 > 3) # True
+print(4 >= 7) # False
+print(10 < 3) # False
+print(5 <= 5) # True
+
+print(3 == 3) # 3과 3이 같다 = True
+print(4 == 2) # False
+print(3 + 4 == 7) # True
+
+print(1 != 3) # 1과 3이 같지 않다 = True
+print(not(1 != 3)) # False
+
+print((3 > 0) and (3 < 5)) # 3이 0보다 크고 3이 5보다 작다 = True
+print((3 > 0) & (3 < 5)) # True
+
+print((3 > 0) or (3 > 5)) #  3이 0보다 크거나 3이 5보다 크다 = Ture
+print((3 > 0) | (3 > 5)) # True
+
+print(5 > 4 > 3) # 연속으로 사용할 땐 AND가 기본. True
+print(5 > 4 > 7) # False
