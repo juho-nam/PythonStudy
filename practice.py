ff0c71cdefc0c9f47222a3149b1cc98d5d47e215
juho-nam <juhonam.dev@gamil.com>
2022-02-21 20:06:37 +0900

    if

--- practice.py
+++ practice.py
@@ -1,18 +1,23 @@
-# 자료구조의 변경
-# 커피숍
-menu = {"커피", "우유", "주스"}
-print(menu) # {'우유', '주스', '커피'}
-print(menu, type(menu)) # {'주스', '커피', '우유'} <class 'set'>
+# if (분기)
+# if 조건:
+#     실행 명령문
 
-menu = list(menu)
-print(menu, type(menu)) # ['우유', '커피', '주스'] <class 'list'>
-# list로 클래스가 바뀜
+weather = input("오늘 날씨는 어떄요? ")
+# 사용자의 입력 받음. input안의 문장 출력 후 입력 받음
+# input은 string 형태로 저장함
+if weather == "비" or weather == "눈":
+    print("우산을 챙기세요")
+elif weather == "미세먼지":
+    print("마스크를 챙기세요")
+else:
+    print("준비물 필요 없어요")
 
-menu = tuple(menu)
-print(menu, type(menu)) # ('주스', '커피', '우유') <class 'tuple'>
-# tuple로 클래스가 바뀜
-
-# 자료구조에 따라서 괄호 모양이 바뀜을 확인하자
-
-menu = set(menu)
-print(menu, type(menu)) # {'커피', '주스', '우유'} <class 'set'>
+temp = int(input("기온은 어때요? "))
+if 30 <= temp:
+    print("너무 더워요")
+elif 10 <= temp and temp < 30:
+    print("괜찮은 날씨에요")
+elif 0 <= temp < 10:
+    print("외투를 챙기세요")
+else:
+    print("너무 추워요. 나가지 마세요")
