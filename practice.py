4914f4e4d20d7903b1a7089a2e594899546ac13d
juho-nam <juhonam.dev@gamil.com>
2022-02-21 19:35:24 +0900

    Set

--- practice.py
+++ practice.py
@@ -1,14 +1,28 @@
-# 튜플 (리스트와 다르게 내용변경,추가는 할 수 없지만 속도가 빠름)
-menu = ("돈까스", "치즈까스")
-print(menu[0])
-print(menu[1])
+# 집합 (set)
+# 중복 안됨, 순서 없음
+my_set = {1, 2, 3, 3, 3}
+print(my_set) # {1, 2, 3}
 
-# menu.add("생선까스") # add가 안 됨
+java = {"유재석", "김태호", "양세형"}
+python = set(["유재석", "박명수"]) # List로 먼저 만들고 set으로 감싸줌
 
-# name = "김종국"
-# age = 20
-# hobby = "코딩"
-# print(name, age, hobby)
+# 교집합 (java와 python을 모두 할 수 있는 개발자)
+print(java & python) # {'유재석'}
+print(java.intersection(python)) # {'유재석'}
 
-(name, age, hobby) = ("김종국", 20, "코딩")
-print(name, age, hobby) # 김종국 20 코딩
+# 합집합 (java 도 할 수 있거나 python 할 수 있는 개발자)
+print(java | python) # {'유재석', '박명수', '김태호', '양세형'}
+print(java.union(python)) # {'유재석', '박명수', '김태호', '양세형'}
+# 순서는 보장되지 않음
+
+# 차집합 (java 할 수 있지만 python은 할 줄 모르는 개발자)
+print(java - python) # {'김태호', '양세형'}
+print(java.difference(python)) # {'김태호', '양세형'}
+
+# python 할 줄 아는 사람이 늘어남
+python.add("김태호")
+print(python) # {'김태호', '박명수', '유재석'}
+
+# java 를 잊어버림
+java.remove("김태호")
+print(java) # {'양세형', '유재석'}
