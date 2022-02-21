b960f256e16dec1238bef4daa5e235b1943bb75a
juho-nam <juhonam.dev@gamil.com>
2022-02-22 03:57:09 +0900

    for loop

--- practice.py
+++ practice.py
@@ -1,23 +1,16 @@
-# if (분기)
-# if 조건:
-#     실행 명령문
+# for
 
-weather = input("오늘 날씨는 어떄요? ")
-# 사용자의 입력 받음. input안의 문장 출력 후 입력 받음
-# input은 string 형태로 저장함
-if weather == "비" or weather == "눈":
-    print("우산을 챙기세요")
-elif weather == "미세먼지":
-    print("마스크를 챙기세요")
-else:
-    print("준비물 필요 없어요")
+for waiting_no in range(5): # 0 에서 5 미만까지
+    print("대기번호 : {0}".format(waiting_no))
+# 대기번호 : 0
+# 대기번호 : 1
+# 대기번호 : 2
+# 대기번호 : 3
+# 대기번호 : 4
 
-temp = int(input("기온은 어때요? "))
-if 30 <= temp:
-    print("너무 더워요")
-elif 10 <= temp and temp < 30:
-    print("괜찮은 날씨에요")
-elif 0 <= temp < 10:
-    print("외투를 챙기세요")
-else:
-    print("너무 추워요. 나가지 마세요")
+starbucks = ["아이언맨", "토르", "그루트"]
+for customer in starbucks:
+    print("{0}, 커피가 준비되었습니다.".format(customer))
+# 아이언맨, 커피가 준비되었습니다.
+# 토르, 커피가 준비되었습니다.
+# 그루트, 커피가 준비되었습니다.
