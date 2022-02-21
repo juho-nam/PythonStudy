c11f1cf812a1640cb822fec130ca924bca8e5d2b
juho-nam <juhonam.dev@gamil.com>
2022-02-22 06:06:48 +0900

    Function

--- practice.py
+++ practice.py
@@ -1,17 +1,7 @@
-# 한 줄 for
+# 함수
+# def 함수이름():
 
-# 출석번호가 1 2 3 4, 앞에 100을 붙이기로 함 -> 101, 102, 103, 104
-students = [1,2,3,4]
-print(students) # [1, 2, 3, 4]
-students = [i+100 for i in students]
-print(students) # [101, 102, 103, 104]
+def open_account():
+    print("새로운 계좌가 생성되엇습니다.")
 
-# 학생 이름을 길이로 변환
-students = ["Iron man", "Thor", "I am groot"]
-students = [len(i) for i in students]
-print(students) # [8, 4, 10]
-
-# 학생 이름을 대문자로 변환
-students = ["Iron man", "Thor", "I am groot"]
-students = [i.upper() for i in students]
-print(students) # ['IRON MAN', 'THOR', 'I AM GROOT']
+open_account() # 함수 호출 해야 실행됨
