ddea5cb967273557445703cbda7915e74b64bbe5
juho-nam <juhonam.dev@gamil.com>
2022-02-22 04:04:49 +0900

    while loop

--- practice.py
+++ practice.py
@@ -1,16 +1,29 @@
-# for
+# while
 
-for waiting_no in range(5): # 0 에서 5 미만까지
-    print("대기번호 : {0}".format(waiting_no))
-# 대기번호 : 0
-# 대기번호 : 1
-# 대기번호 : 2
-# 대기번호 : 3
-# 대기번호 : 4
+customer = "토르"
+index = 5
+while index >= 1:
+    print("{0}, 커피가 준비되었습니다. {1} 번 남았어요.".format(customer, index))
+    index -= 1
+    if index == 0:
+        print("커피는 폐기되었습니다.")
+# 토르, 커피가 준비되었습니다. 5 번 남았어요.
+# 토르, 커피가 준비되었습니다. 4 번 남았어요.
+# 토르, 커피가 준비되었습니다. 3 번 남았어요.
+# 토르, 커피가 준비되었습니다. 2 번 남았어요.
+# 토르, 커피가 준비되었습니다. 1 번 남았어요.
+# 커피는 폐기되었습니다.
 
-starbucks = ["아이언맨", "토르", "그루트"]
-for customer in starbucks:
+# customer = "아이언맨"
+# index = 1
+# while True:
+#     print("{0}, 커피가 준비되었습니다. {1} 번 남았어요.".format(customer, index))
+#     index += 1
+# 무한루프. 종료하려면 ctrl + c
+
+person = "Unknown"
+
+while person != customer:
     print("{0}, 커피가 준비되었습니다.".format(customer))
-# 아이언맨, 커피가 준비되었습니다.
-# 토르, 커피가 준비되었습니다.
-# 그루트, 커피가 준비되었습니다.
+    person = input("이름이 어떻게 되세요? ")
+# 이름이 토르가 나올때까지 반복
