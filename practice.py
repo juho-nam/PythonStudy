84c8e319c413bfa2faf30e56b5da6684684f5dfa
juho-nam <juhonam.dev@gamil.com>
2022-02-21 19:27:26 +0900

    Tuple

--- practice.py
+++ practice.py
@@ -1,47 +1,14 @@
-# 사전
-cabinet = {3: "유재석", 100:"김태호"} # ket:value
-print(cabinet[3]) # 유재석
-print(cabinet[100]) # 김태호
+# 튜플 (리스트와 다르게 내용변경,추가는 할 수 없지만 속도가 빠름)
+menu = ("돈까스", "치즈까스")
+print(menu[0])
+print(menu[1])
 
-print(cabinet.get(3)) # 유재석
-# print(cabinet[5]) # KeyError : 5
+# menu.add("생선까스") # add가 안 됨
 
-# .get()을 이용하면 None을 반환하고 다음 줄로 넘어간다.
-print(cabinet.get(5)) # None
+# name = "김종국"
+# age = 20
+# hobby = "코딩"
+# print(name, age, hobby)
 
-# key 5 에 값이 있으면 값을 불러옴.
-print(cabinet.get(5, "사용가능")) # 사용가능
-
-# 사전 자료 안에 값이 있는지를 확인
-print(3 in cabinet) # True
-print(5 in cabinet) # False
-
-# 정수가 아닌 string 형으로 선언도 가능
-cabinet = {"A-3":"유재석","B-100":"김태호"}
-print(cabinet["A-3"]) # 유재석
-print(cabinet["B-100"]) # 김태호
-
-# 새 손님
-print(cabinet) # {'A-3': '유재석', 'B-100': '김태호'}
-# C-20 이라는 key를 만들고 조세호 라는 value를 넣음
-# C-20에 이미 값이 있으면 업데이트 됨
-cabinet["C-20"] = "조세호"
-cabinet["A-3"] = "김종국"
-print(cabinet) # {'A-3': '김종국', 'B-100': '김태호', 'C-20': '조세호'}
-
-# 손님 퇴장
-del cabinet["A-3"]
-print(cabinet) # {'B-100': '김태호', 'C-20': '조세호'}
-
-# key 들만 출력
-print(cabinet.keys()) # dict_keys(['B-100', 'C-20'])
-
-# value 들만 출력
-print(cabinet.values()) # dict_values(['김태호', '조세호'])
-
-# key, value 쌍으로 출력
-print(cabinet.items()) # dict_items([('B-100', '김태호'), ('C-20', '조세호')])
-
-# 목용탕 영업 종료
-cabinet.clear()
-print(cabinet) # {}
+(name, age, hobby) = ("김종국", 20, "코딩")
+print(name, age, hobby) # 김종국 20 코딩
