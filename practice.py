1a61bc583d859cfcce5f556e9f2af42c61e8c504
juho-nam <juhonam.dev@gamil.com>
2022-02-22 04:11:35 +0900

    continue, break

--- practice.py
+++ practice.py
@@ -1,29 +1,12 @@
-# while
+# continue 와 break
+# 반복문 내에서 사용
 
-customer = "토르"
-index = 5
-while index >= 1:
-    print("{0}, 커피가 준비되었습니다. {1} 번 남았어요.".format(customer, index))
-    index -= 1
-    if index == 0:
-        print("커피는 폐기되었습니다.")
-# 토르, 커피가 준비되었습니다. 5 번 남았어요.
-# 토르, 커피가 준비되었습니다. 4 번 남았어요.
-# 토르, 커피가 준비되었습니다. 3 번 남았어요.
-# 토르, 커피가 준비되었습니다. 2 번 남았어요.
-# 토르, 커피가 준비되었습니다. 1 번 남았어요.
-# 커피는 폐기되었습니다.
-
-# customer = "아이언맨"
-# index = 1
-# while True:
-#     print("{0}, 커피가 준비되었습니다. {1} 번 남았어요.".format(customer, index))
-#     index += 1
-# 무한루프. 종료하려면 ctrl + c
-
-person = "Unknown"
-
-while person != customer:
-    print("{0}, 커피가 준비되었습니다.".format(customer))
-    person = input("이름이 어떻게 되세요? ")
-# 이름이 토르가 나올때까지 반복
+absent = [2, 5] # 결석
+no_book = [7] # 책 없음
+for student in range(1, 11): # 1~10
+    print("{0}, 책을 일어봐".format(student))
+    if student in absent:
+        continue # 다음 문장을 실행시키지 않고 바로 반복문 조건 확인
+    elif student in no_book:
+        print("오늘 수업 여기까지. {0}는 교무실로 따라와".format(student))
+        break # 반복문 종료
