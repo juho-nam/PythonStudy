b1f31668e69efed75389bf666a73a369192b59b2
juho-nam <juhonam.dev@gamil.com>
2022-02-22 04:16:58 +0900

    One line 'for'

--- practice.py
+++ practice.py
@@ -1,12 +1,17 @@
-# continue 와 break
-# 반복문 내에서 사용
+# 한 줄 for
 
-absent = [2, 5] # 결석
-no_book = [7] # 책 없음
-for student in range(1, 11): # 1~10
-    print("{0}, 책을 일어봐".format(student))
-    if student in absent:
-        continue # 다음 문장을 실행시키지 않고 바로 반복문 조건 확인
-    elif student in no_book:
-        print("오늘 수업 여기까지. {0}는 교무실로 따라와".format(student))
-        break # 반복문 종료
+# 출석번호가 1 2 3 4, 앞에 100을 붙이기로 함 -> 101, 102, 103, 104
+students = [1,2,3,4]
+print(students) # [1, 2, 3, 4]
+students = [i+100 for i in students]
+print(students) # [101, 102, 103, 104]
+
+# 학생 이름을 길이로 변환
+students = ["Iron man", "Thor", "I am groot"]
+students = [len(i) for i in students]
+print(students) # [8, 4, 10]
+
+# 학생 이름을 대문자로 변환
+students = ["Iron man", "Thor", "I am groot"]
+students = [i.upper() for i in students]
+print(students) # ['IRON MAN', 'THOR', 'I AM GROOT']
