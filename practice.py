02392f674db848b4bd4e62f8f70ff012fbc88a87
juho-nam <juhonam.dev@gamil.com>
2022-02-22 18:55:27 +0900

    with

--- practice.py
+++ practice.py
@@ -1,14 +1,16 @@
-# Pickle
-# 프로그램 상에서 사용하는 데이터를 파일 형태로 저장
+# with
 
 import pickle
-profile_file = open("profile.pickle", "wb") # b는 binary를 뜻함. 피클을 쓰기 위해선 항상 binary로 해야함
-profile = {"이름":"박명수","나이":30,"취미":["축구", "골프","코딩"]}
-print(profile) # {'이름': '박명수', '나이': 30, '취미': ['축구', '골프', '코딩']}
-pickle.dump(profile, profile_file) # profile 에 있는 정보를 file 에 저장
-profile_file.close()
 
-profile_file = open("profile.pickle", "rb")
-profile = pickle.load(profile_file) # file 에 있는 정보를 profile 에 불러오기
-print(profile) # {'이름': '박명수', '나이': 30, '취미': ['축구', '골프', '코딩']}
-profile_file.close()
+# with open("profile.pickle", "rb") as profile_file:
+#     print(pickle.load(profile_file))
+
+# profile.pickle 을 열어서 profile_file 로 저장을 해두고
+# profile_file 내용을 pickle.load 로 불러와서 출력
+# close() 가 필요없이 with 문에서 자동으로 close 해줌
+
+with open("study.txt","w",encoding="utf8") as study_file:
+    study_file.write("파이썬을 열심히 공부하고 있어요")
+
+with open("study.txt","r",encoding="utf8") as study_file:
+    print(study_file.read()) # 파이썬을 열심히 공부하고 있어요
