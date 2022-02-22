3a105c707a89c5d449580666ed39bf3500d96d13
juho-nam <juhonam.dev@gamil.com>
2022-02-22 19:28:10 +0900

    Member variable

--- practice.py
+++ practice.py
@@ -1,19 +1,23 @@
-# __init__
-# 파이썬에서 쓰이는 생성자
-# __init__ 함수에 정의된 (self 를 제외한 나머지) 인자들의 수와 동일하게 해야함
-# marine1,2 와 tank 는 unit 클래스의 '객체'
-
+# 멤버변수
+# 클래스 내에서 정의된 변수
+# self.name, self.hp, self.damage 등이 멤버변수
+# 파이썬에서는 외부에서 객체에 추가로 변수를 만들어 쓸 수 있음
+# 확장한 변수는 확장된 객체에서만 사용 가능
 class unit:
     def __init__(self, name, hp, damage):
         self.name = name # 유닛 이름
         self.hp = hp # 유닛 체력
         self.damage = damage # 유닛 공격력
         print("{0} 유닛이 생성 되었습니다.".format(self.name))
-        print("체력 {0}, 공격력 {1}\n".format(self.hp,self.damage))
+        print("체력 {0}, 공격력 {1}".format(self.hp,self.damage))
 
-# marine1 = unit("마린", 40, 5)
-# marine2 = unit("마린", 40, 5)
-# tank = unit("탱크", 150, 35)
+# 레이스 : 공중 유닛, 비행기, 클로킹(상대방에게 보이지 않음)
+wraith1 = unit("레이스", 80, 5)
+print("유닛 이름 : {0}, 공격력 : {1}".format(wraith1.name, wraith1.damage))
 
-marine1 = unit("마린", 40)
-# TypeError: unit.__init__() missing 1 required positional argument: 'damage'
+# 마인드 컨트롤 : 상대방 유닛을 내 것으로 만드는 것 (빼앗음)
+wraith2 = unit("빼앗은 레이스", 80, 5)
+wraith2.clocking = True # 외부에서 clocking 이라는 변수를 추가로 할당
+
+if wraith2.clocking == True:
+    print("{0} 는 현재 클로킹 상태입니다.".format(wraith2.name))
