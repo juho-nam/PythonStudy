bf609a6a18c42abd5f5722e92041189aa9859df3
juho-nam <juhonam.dev@gamil.com>
2022-02-22 19:37:51 +0900

    Method

--- practice.py
+++ practice.py
@@ -1,9 +1,6 @@
-# 멤버변수
-# 클래스 내에서 정의된 변수
-# self.name, self.hp, self.damage 등이 멤버변수
-# 파이썬에서는 외부에서 객체에 추가로 변수를 만들어 쓸 수 있음
-# 확장한 변수는 확장된 객체에서만 사용 가능
-class unit:
+# 메소드
+
+class Unit:
     def __init__(self, name, hp, damage):
         self.name = name # 유닛 이름
         self.hp = hp # 유닛 체력
@@ -11,13 +8,34 @@ class unit:
         print("{0} 유닛이 생성 되었습니다.".format(self.name))
         print("체력 {0}, 공격력 {1}".format(self.hp,self.damage))
 
-# 레이스 : 공중 유닛, 비행기, 클로킹(상대방에게 보이지 않음)
-wraith1 = unit("레이스", 80, 5)
-print("유닛 이름 : {0}, 공격력 : {1}".format(wraith1.name, wraith1.damage))
+# 공격 유닛
+class Attackunit:
+        def __init__(self, name, hp, damage):
+            self.name = name
+            self.hp = hp
+            self.damage = damage
 
-# 마인드 컨트롤 : 상대방 유닛을 내 것으로 만드는 것 (빼앗음)
-wraith2 = unit("빼앗은 레이스", 80, 5)
-wraith2.clocking = True # 외부에서 clocking 이라는 변수를 추가로 할당
+        def attack(self, location):
+            print("{0} : {1} 방향으로 적군을 공격 합니다. [공격력 {2}]"\
+                .format(self.name, location, self.damage)) # location은 전달받은 값을 사용
 
-if wraith2.clocking == True:
-    print("{0} 는 현재 클로킹 상태입니다.".format(wraith2.name))
+        def damaged(self, damage):
+            print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
+            self.hp -= damage
+            print("{0} : 현재 체력은 {1} 입니다.".format(self.name, self.hp))
+            if self.hp <= 0:
+                print("{0} : 파괴되었습니다.".format(self.name))
+
+# 파이어뱃 : 공격 유닛, 화염방사기
+firebat1 = Attackunit("파이어뱃", 50, 16)
+firebat1.attack("5시")
+# 파이어뱃 : 5시 방향으로 적군을 공격 합니다. [공격력 16]
+
+# 공격 2번 받는다고 가정
+firebat1.damaged(25)
+# 파이어뱃 : 25 데미지를 입었습니다.
+# 파이어뱃 : 현재 체력은 25 입니다. 
+firebat1.damaged(25)
+# 파이어뱃 : 25 데미지를 입었습니다.
+# 파이어뱃 : 현재 체력은 0 입니다.  
+# 파이어뱃 : 파괴되었습니다.
