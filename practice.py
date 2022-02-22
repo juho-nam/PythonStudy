805354890cdd194c54a175c145dae256d2bf31da
juho-nam <juhonam.dev@gamil.com>
2022-02-22 20:49:25 +0900

    Method overriding

--- practice.py
+++ practice.py
@@ -1,16 +1,22 @@
-# 다중 상속
-# Unit 은 부모 클래스, AttackUnit 은 자식 클래스
+# 메소드 오버라이딩
+# 메소드 선언부는 기존 메소드와 완전이 같아야 함
 
 # 일반 유닛
 class Unit:
-    def __init__(self, name, hp):
+    def __init__(self, name, hp, speed):
         self.name = name # 유닛 이름
         self.hp = hp # 유닛 체력
+        self.speed = speed
+
+    def move(self, location):
+        print("[지상 유닛 이동]")
+        print("{0} : {1} 방향으로 이동합니다. [속도 {2}]"\
+            .format(self.name, location, self.speed))
 
 # 공격 유닛
 class AttackUnit(Unit): # Attackunit 은 Unit 을 상속받아 만들어짐
-        def __init__(self, name, hp, damage):
-            Unit.__init__(self, name, hp)
+        def __init__(self, name, hp, speed, damage):
+            Unit.__init__(self, name, hp, speed)
             self.damage = damage
 
         def attack(self, location):
@@ -24,8 +30,6 @@ class AttackUnit(Unit): # Attackunit 은 Unit 을 상속받아 만들어짐
             if self.hp <= 0:
                 print("{0} : 파괴되었습니다.".format(self.name))
 
-# 드랍쉽 : 공중 유닛, 수송기. 마린 / 파이어뱃 / 탱크 등을 수송. 공격 X
-
 # 날 수 있는 기능을 가진 클래스
 class Flyable:
     def __init__(self, flying_speed):
@@ -38,10 +42,23 @@ class Flyable:
 # 공중 공격 유닛 클래스
 class FlyableAttackUnit(AttackUnit, Flyable):
     def __init__(self, name, hp, damage, flying_speed):
-        AttackUnit.__init__(self, name, hp, damage)
+        AttackUnit.__init__(self, name, hp, 0, damage) # 지상 speed 0
         Flyable.__init__(self, flying_speed)
 
-# 발키리 : 공중 공격 유닛, 한번에 14발 미사일 발사
-valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)
-valkyrie.fly(valkyrie.name, "3시")
-# 발키리 : 3시 방향으로 날아갑니다. [속도 5]
+    def move(self, location): # move 함수를 재정의
+        print("[공중 유닛 이동]")
+        self.fly(self.name, location)
+
+# 벌쳐 : 지상 유닛, 기동성이 좋음
+vulture = AttackUnit("벌쳐", 80, 10 ,20)
+
+# 배틀크루저 : 공중 유닛, 체력도 굉장히 좋음, 공격력도 좋음
+battlecruiser = FlyableAttackUnit("배틀크루저", 500, 25, 3)
+
+vulture.move("11시")
+# [지상 유닛 이동]
+# 벌쳐 : 11시 방향으로 이동합니다. [속도 10]
+
+battlecruiser.move("9시")
+# [공중 유닛 이동]
+# 배틀크루저 : 9시 방향으로 날아갑니다. [속도 3]
