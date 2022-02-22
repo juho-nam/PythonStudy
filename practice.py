159996f3b9d2ceb2d5ea56fe1274185805760df5
juho-nam <juhonam.dev@gamil.com>
2022-02-22 18:46:03 +0900

    Pickle

--- practice.py
+++ practice.py
@@ -1,54 +1,14 @@
-# 파일입출력
+# Pickle
+# 프로그램 상에서 사용하는 데이터를 파일 형태로 저장
 
-# score_file = open("score.txt", "w", encoding="utf8") # "w"는 쓰기. 덮이쓰기
-# print("수학 : 0", file=score_file)
-# print("영어 : 50", file=score_file)
-# score_file.close()
+import pickle
+profile_file = open("profile.pickle", "wb") # b는 binary를 뜻함. 피클을 쓰기 위해선 항상 binary로 해야함
+profile = {"이름":"박명수","나이":30,"취미":["축구", "골프","코딩"]}
+print(profile) # {'이름': '박명수', '나이': 30, '취미': ['축구', '골프', '코딩']}
+pickle.dump(profile, profile_file) # profile 에 있는 정보를 file 에 저장
+profile_file.close()
 
-# score_file = open("score.txt", "a", encoding="utf8") # "a"는 append. 뒤에 이어 쓰기
-# score_file.write("과학 : 80")
-# score_file.write("\n코딩 : 100")
-# score_file.close()
-
-# score_file = open("score.txt", "r", encoding="utf8")
-# print(score_file.read()) # .read는 파일의 내용을 모두 읽어옴
-# score_file.close()
-# 수학 : 0
-# 영어 : 50
-# 과학 : 80
-# 코딩 : 100
-
-# score_file = open("score.txt", "r", encoding="utf8")
-# print(score_file.readline(), end="") # 줄별로 읽기, 한 줄 읽고 커서는 다음 줄로 이동
-# print(score_file.readline(), end="")
-# print(score_file.readline(), end="")
-# print(score_file.readline(), end="")
-# score_file.close()
-# 수학 : 0
-# 영어 : 50
-# 과학 : 80
-# 코딩 : 100
-
-# 몇 줄인지 모를 때 하는 방법
-# score_file = open("score.txt", "r", encoding="utf8")
-# while True:
-#     line = score_file.readline()
-#     if not line:
-#         break
-#     print(line,end="")
-# score_file.close()
-# 수학 : 0
-# 영어 : 50
-# 과학 : 80
-# 코딩 : 100
-
-# 리스트에 값을 넣어 처리
-score_file = open("score.txt", "r", encoding="utf8")
-lines = score_file.readlines() # 모든 라인을 list 형태로 저장
-for line in lines:
-    print(line, end="")
-score_file.close()
-# 수학 : 0
-# 영어 : 50
-# 과학 : 80
-# 코딩 : 100
+profile_file = open("profile.pickle", "rb")
+profile = pickle.load(profile_file) # file 에 있는 정보를 profile 에 불러오기
+print(profile) # {'이름': '박명수', '나이': 30, '취미': ['축구', '골프', '코딩']}
+profile_file.close()
