67b147acdade96be75467fc79c9b9f056c8a937f
juho-nam <juhonam.dev@gamil.com>
2022-02-22 20:38:10 +0900

    Multiple inheritance

--- practice.py
+++ practice.py
@@ -1,4 +1,5 @@
-# 상속
+# 다중 상속
+# Unit 은 부모 클래스, AttackUnit 은 자식 클래스
 
 # 일반 유닛
 class Unit:
@@ -7,7 +8,7 @@ class Unit:
         self.hp = hp # 유닛 체력
 
 # 공격 유닛
-class Attackunit(Unit): # Attackunit 은 Unit 을 상속받아 만들어짐
+class AttackUnit(Unit): # Attackunit 은 Unit 을 상속받아 만들어짐
         def __init__(self, name, hp, damage):
             Unit.__init__(self, name, hp)
             self.damage = damage
@@ -23,18 +24,24 @@ class Attackunit(Unit): # Attackunit 은 Unit 을 상속받아 만들어짐
             if self.hp <= 0:
                 print("{0} : 파괴되었습니다.".format(self.name))
 
-# 메딕 : 의무병
+# 드랍쉽 : 공중 유닛, 수송기. 마린 / 파이어뱃 / 탱크 등을 수송. 공격 X
 
-# 파이어뱃 : 공격 유닛, 화염방사기
-firebat1 = Attackunit("파이어뱃", 50, 16)
-firebat1.attack("5시")
-# 파이어뱃 : 5시 방향으로 적군을 공격 합니다. [공격력 16]
+# 날 수 있는 기능을 가진 클래스
+class Flyable:
+    def __init__(self, flying_speed):
+        self.flying_speed = flying_speed
 
-# 공격 2번 받는다고 가정
-firebat1.damaged(25)
-# 파이어뱃 : 25 데미지를 입었습니다.
-# 파이어뱃 : 현재 체력은 25 입니다.
-firebat1.damaged(25)
-# 파이어뱃 : 25 데미지를 입었습니다.
-# 파이어뱃 : 현재 체력은 0 입니다.
-# 파이어뱃 : 파괴되었습니다.
+    def fly(self, name, location):
+        print("{0} : {1} 방향으로 날아갑니다. [속도 {2}]"\
+            .format(name, location, self.flying_speed))
+
+# 공중 공격 유닛 클래스
+class FlyableAttackUnit(AttackUnit, Flyable):
+    def __init__(self, name, hp, damage, flying_speed):
+        AttackUnit.__init__(self, name, hp, damage)
+        Flyable.__init__(self, flying_speed)
+
+# 발키리 : 공중 공격 유닛, 한번에 14발 미사일 발사
+valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)
+valkyrie.fly(valkyrie.name, "3시")
+# 발키리 : 3시 방향으로 날아갑니다. [속도 5]
